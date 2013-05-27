4bd82c0a5ba4ff942e76fc74b4ed3a1474cd27b8
Masaaki Goshima <goccy54@gmail.com>
2013-05-27 16:58:07 +0900

    this version was uploaded CPAN as 0.12

--- lib/Compiler/Lexer.pm
+++ lib/Compiler/Lexer.pm
@@ -10,7 +10,7 @@ our @ISA = qw(Exporter);
 our %EXPORT_TAGS = ( 'all' => [ qw() ] );
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = qw();
-our $VERSION = '0.09';
+our $VERSION = '0.12';
 require XSLoader;
 XSLoader::load(__PACKAGE__, $VERSION);
 
