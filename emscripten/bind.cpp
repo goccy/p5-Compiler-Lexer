31b2b7e802abe6cad6441f5c8be8ea6b9464824c
Masaaki Goshima <goccy54@gmail.com>
2015-02-25 10:42:45 +0900

    update Makefile

--- emscripten/bind.cpp
+++ emscripten/bind.cpp
@@ -3,14 +3,57 @@
 
 using namespace emscripten;
 
-Tokens *tokenize(char *script)
+class CompilerLexerToken {
+public:
+	std::string data;
+	std::string name;
+    CompilerLexerToken();
+    void setData(std::string data);
+    const std::string& getData();
+    void setName(std::string name);
+    const std::string& getName();
+};
+
+CompilerLexerToken::CompilerLexerToken()
+{
+}
+
+void CompilerLexerToken::setData(std::string data)
+{
+	this->data = data;
+}
+
+const std::string &CompilerLexerToken::getData()
+{
+	return this->data;
+}
+
+void CompilerLexerToken::setName(std::string name)
+{
+	this->name = name;
+}
+
+const std::string& CompilerLexerToken::getName()
+{
+	return this->name;
+}
+
+CompilerLexerToken tokenize(std::string script)
 {
 	Lexer lexer("", false);
-	return lexer.tokenize(script);
+	Tokens *tokens = lexer.tokenize((char *)script.c_str());
+	CompilerLexerToken *token = new CompilerLexerToken();
+	if (tokens->size() == 0) return *token;
+	token->name = std::string(((Token *)tokens->at(0))->info.name);
+	token->data = std::string(((Token *)tokens->at(0))->_data);
+	return *token;
 }
 
 EMSCRIPTEN_BINDINGS(compiler_lexer) {
-	class_<Token>("Token");
-	class_<std::vector<Token*>>register_vector("Tokens");
-	function("tokenize", &tokenize, allow_raw_pointers());
+	class_<CompilerLexerToken>("Token").constructor()
+		.function("setData", &CompilerLexerToken::setData)
+		.function("getData", &CompilerLexerToken::getData)
+		.function("setName", &CompilerLexerToken::setName)
+		.function("getName", &CompilerLexerToken::getName);
+	function("tokenize", &tokenize);
 }
